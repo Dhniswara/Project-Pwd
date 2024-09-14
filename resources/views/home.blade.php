a8d1b9100ac9f54ae2a36e8c175228f61e98b6e9
Dhniswara <dhaniswarafadhlurahman@gmail.com>
2024-09-14 08:05:45 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CinemiX</title>
+
+    <!-- LINK START-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=New+Amsterdam&family=Playwrite+US+Modern:wght@100..400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/app.css">
+    <!-- LINK END -->
+    
+</head>
+<body>
+    
+    <!-- HEADER -->
+    <header class="header" >
+        <a href="#" class="logo">Cinemi<span>X</span></a>
+        <nav>
+            <a href="">Home</a>
+            <a href="#movie">Movies</a>
+            <a href="">TV Shows</a>
+            <a href="">Hollywood</a>
+            <a href="">Horror</a>
+        </nav>
+        <div class="icon">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-bell"></i>
+            <i class="fa-solid fa-user"></i>
+        </div>
+    </header>
+    <!-- HEADER END -->
+
+    
+    <section class="home">
+        <div class="movie_container">
+            <div class="movie_wrapper">
+                <div class="movie_box mySwiper">
+                    <div class="movie_content swiper-wrapper">
+                        <div class="movie_slide swiper-slide" id="first">
+                            <div class="bg" >
+                            <h3>UNCHARTED</h3>
+
+                            <p>Street-smart Nathan Drake (Tom Holland) is recruited by seasoned treasure 
+                            hunter Victor "Sully" Sullivan (Mark Wahlberg) to recover a fortune amassed by 
+                            Ferdinand Magellan and lost 500 years ago by the House of Moncada.
+                            Street-smart Nathan Drake (Tom Holland) is recruited by seasoned treasure 
+                            hunter Victor "Sully" Sullivan (Mark Wahlberg) to recover a fortune amassed by 
+                            Ferdinand Magellan and lost 500 years ago by the House of Moncada.</p>
+                            <div class="movie_review">
+                                <h3>IMDb : 6,3</h3>
+                                <h3>1 h 56 m</h3>
+                                <h3>10+</h3>
+                            </div>
+                            <div class="btn_ctn">
+                                <div class="btn" id="btn">Read More</div>
+                                <div class="btn">Add List</div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="movie_slide swiper-slide" id="second">
+                            <div class="bg">
+                            <h3>UNCHARTED</h3>
+
+                            <p>Street-smart Nathan Drake (Tom Holland) is recruited by seasoned treasure 
+                            hunter Victor "Sully" Sullivan (Mark Wahlberg) to recover a fortune amassed by 
+                            Ferdinand Magellan and lost 500 years ago by the House of Moncada.
+                            Street-smart Nathan Drake (Tom Holland) is recruited by seasoned treasure 
+                            hunter Victor "Sully" Sullivan (Mark Wahlberg) to recover a fortune amassed by 
+                            Ferdinand Magellan and lost 500 years ago by the House of Moncada.</p>
+                            <div class="movie_review">
+                                <h3>IMDb : 6,3</h3>
+                                <h3>1 h 56 m</h3>
+                                <h3>10+</h3>
+                            </div>
+                            <div class="btn_ctn">
+                                <div class="btn">Read More</div>
+                                <div class="btn">Add List</div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="movie_slide swiper-slide" id="third">
+                            <div class="bg">
+                            <h3>UNCHARTED</h3>
+
+                            <p>Street-smart Nathan Drake (Tom Holland) is recruited by seasoned treasure 
+                            hunter Victor "Sully" Sullivan (Mark Wahlberg) to recover a fortune amassed by 
+                            Ferdinand Magellan and lost 500 years ago by the House of Moncada.
+                            Street-smart Nathan Drake (Tom Holland) is recruited by seasoned treasure 
+                            hunter Victor "Sully" Sullivan (Mark Wahlberg) to recover a fortune amassed by 
+                            Ferdinand Magellan and lost 500 years ago by the House of Moncada.</p>
+                            <div class="movie_review">
+                                <h3>IMDb : 6,3</h3>
+                                <h3>1 h 56 m</h3>
+                                <h3>10+</h3>
+                            </div>
+                            <div class="btn_ctn">
+                                <div class="btn">Read More</div>
+                                <div class="btn">Add List</div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+    </section>
+
+    <section class="movie" id="movie">
+        <h1 style="color: var(--red);">Latest Movie</h1>
+        <div class="Latest_container">
+            
+            <div class="latest_box">
+            <a href="tampilan">
+                <img src="images/l-6.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Kin 2018</p>
+                    <p>Action Movie</p>
+                </div>
+            </a>
+            </div>
+        
+            <div class="latest_box">
+                <img src="images/l-3.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Croods</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+            <div class="latest_box">
+                <img src="images/l-4.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Toy Story</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+            <div class="latest_box">
+                <img src="images/l-1.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Kin 2018</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+            <div class="latest_box">
+                <img src="images/l-1.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Kin 2018</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+        </div>
+        <h1 style="color: var(--red); margin-top: 30px;">Movie</h1>
+        <div class="Latest_container">
+            <div class="latest_box">
+                <img src="images/l-1.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Kin 2018</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+            <div class="latest_box">
+                <img src="images/l-1.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Kin 2018</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+            <div class="latest_box">
+                <img src="images/l-1.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Kin 2018</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+            <div class="latest_box">
+                <img src="images/l-1.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Kin 2018</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+            <div class="latest_box">
+                <img src="images/l-1.jpg" alt="">
+                <div class="desc_movie">
+                    <p>Kin 2018</p>
+                    <p>Action Movie</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+
+        <div class="footer">
+            <div class="cinemax">
+                <h2 class="cinemi">Cinemi<span>X</span></h2>
+                    <div class="emcin">
+                        <i class='bx bx-envelope bx-sm'></i>
+                        <h5>Cinemix@gmail.com</h5>
+                    </div>
+
+                    <div class="emcin">
+                        <i class='bx bxs-phone bx-sm'></i>
+                        <h5>+62 877 8628 1379 </h5>
+                    </div>
+         </div>
+
+                <div class="company">
+                    <h2>Company</h2>
+                        <div class="component">
+                            <h2>About us</h2>
+                            <h2>Privacy Policy</h2>
+                            <h2>Contact us</h2>
+                        </div>
+                </div>
+
+                <div class="premium-movie">
+                    <h2>Premium Movies</h2>
+                    <div class="component">
+                        <h2>Primal</h2>
+                        <h2>Dead end</h2>
+                        <h2>Jewel</h2>
+                    </div>
+                </div>
+
+                    <div class="quick-links">
+                        <h2>Quick Links</h2>
+                     <div class="component">
+                            <h2>Terms Of Us</h2>
+                            <h2>Error</h2>
+                            <h2>Share Feedback</h2>
+                    </div>
+                </div>
+        </div>
+
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ resources/views/tampilan.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Film </title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/tampilan.css">
+</head>
+<body>
+    
+    <div class="back">
+        <a href="home"><i class='bx bx-left-arrow-alt bx-lg' ></i></a>
+    </div>
+
+    <div class="judul">
+        <h1>STAR WARS</h1>
+    </div>
+
+    <div class="logo-film">
+            <img src="images/l-6.jpg" alt="">
+                <div class="keterangan">
+                    <h3>Country : USA</h3>
+                    <h3>Movie Star : <span>Nicole Alonso,  Tommy Ball,  Torey Byrne,  Will Crount</h3>
+                    <h3>Director : Steven Mars</h3>
+                    <h3>Genre : Action, Battle</h3>
+                    <h3>IMDb : 8,8 </h3>
+                    <h3>Published : December 27, 2022</h3>
+                    <h3>Duration : 2 Hours 35 Minutes</h3>
+                    <h3 id="sinopsis">Sinopsis : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut quisquam esse voluptates, nisi ratione adipisci porro provident libero! Et, dolore. Perferendis maxime qui aperiam cumque molestiae necessitatibus aut magni cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum provident rem quia asperiores, sed adipisci laboriosam, alias quos, modi vero expedita molestias quod labore iusto eius velit cum quibusdam totam?</h3>
+                 </div>
+    </div>
+
+    <div class="rating">
+        <div class="star">
+            <i class='bx bxs-star bx-sm'></i>
+            <i class='bx bxs-star bx-sm'></i>
+            <i class='bx bxs-star bx-sm'></i>
+            <i class='bx bxs-star bx-sm'></i>
+            <i class='bx bxs-star bx-sm'></i>
+            
+        </div>
+
+        <div class="rating-number">
+            <h4>5.0</h4>
+            
+        </div>
+
+        <div class="bookmark">
+            <i class='bx bxs-bookmark bx-md'></i>
+        </div>
+    </div>
+
+</body>
+</html>
